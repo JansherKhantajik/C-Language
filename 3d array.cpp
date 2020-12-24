e18d83f2cc98a04888c9d57f6be3f5a833d3ad4c
Jansher Khan <76558654+JansherKhantajik@users.noreply.github.com>
2020-12-24 17:17:02 +0500

    Add files via upload

---
+++ 3d array.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	/* Take 10 number input by user and cheak and print the repeated value*/
+	int num[10];
+	int i;
+	
+	for (i=0; i<=9; i++)
+	{
+		printf("Enter Numbers ");
+		scanf("%d",&num[i]);
+	}
+	
+	
+	
+	
+	 
+}
